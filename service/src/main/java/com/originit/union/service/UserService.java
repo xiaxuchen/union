a2dace186e7e0a3ad88a78976dedbfd53080dcb7
Super丶执念 <956822950@qq.com>
2020-02-06 20:41:54 +0800

    //定义UserBind接口

--- service/src/main/java/com/originit/union/service/UserService.java
+++ service/src/main/java/com/originit/union/service/UserService.java
@@ -4,8 +4,12 @@ import com.baomidou.mybatisplus.extension.service.IService;
 import com.originit.union.entity.UserBindEntity;
 import com.originit.union.mapper.UserDao;
 
+import java.util.List;
+
 /**
  * 访问用户相关表的Service
  */
 public interface UserService extends IService<UserBindEntity> {
+    //更新会员手机绑定信息
+    public  String updateUserBind(List<UserBindEntity> userBindEntityList);
 }
