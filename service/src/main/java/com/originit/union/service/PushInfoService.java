2b79e16580d701e29f78de0f394a3f8499523892
Super丶执念 <956822950@qq.com>
2020-02-11 17:55:02 +0800

    //2020.2.11pushinfo接口完成

--- service/src/main/java/com/originit/union/service/PushInfoService.java
+++ service/src/main/java/com/originit/union/service/PushInfoService.java
@@ -2,9 +2,22 @@ package com.originit.union.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.originit.union.entity.PushInfoEntity;
+import com.originit.union.entity.dto.PushInfoDto;
+import com.soecode.wxtools.exception.WxErrorException;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.IOException;
+import java.util.List;
 
 /**
  * 通过此类可以访问推送消息表
  */
 public interface PushInfoService extends IService<PushInfoEntity> {
+    /**
+     *添加用户推送信息
+     * @param openidList 用户列表
+     * @param pushInfoDto  推送的信息，type为1表示文本消息，为2表示图文消息，content对应为文本内容和微信公众平台的media_id
+     */
+     void addPushInfo(List<String> openidList, PushInfoDto pushInfoDto);
 }
