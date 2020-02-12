74c2b408afdeaa29a03b9d433564504fa05e88b9
xxc <17770092259@163.com>
2020-02-12 23:19:28 +0800

    尝试了Springboot整合vue，添加了MapStruct,将端口修改为8080

--- api/src/main/java/com/originit/union/api/config/MvcConfig.java
+++ api/src/main/java/com/originit/union/api/config/MvcConfig.java
@@ -3,11 +3,18 @@ package com.originit.union.api.config;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.HashSet;
 import java.util.List;
 
 @Configuration
@@ -38,4 +45,12 @@ public class MvcConfig implements WebMvcConfigurer {
         converters.add(converter);
     }
 
+    @Bean
+    public ConfigurableServletWebServerFactory webServerFactory () {
+        final TomcatServletWebServerFactory tomcatServletWebServerFactory = new TomcatServletWebServerFactory();
+        HashSet<ErrorPage> set = new HashSet<>();
+        set.add(new ErrorPage(HttpStatus.NOT_FOUND,"/index.html"));
+        tomcatServletWebServerFactory.setErrorPages(set);
+        return tomcatServletWebServerFactory;
+    }
 }
