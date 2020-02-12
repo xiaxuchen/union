2b387a3299531a20fb6c9bcf080188131189f556
xxc <17770092259@163.com>
2020-02-12 18:56:35 +0800

    使用springboot的方式配置跨域，添加定时任务框架quartz，添加不同环境下的配置文件，修改pom.xml使之能够打包

--- api/src/main/java/com/originit/union/api/config/MvcConfig.java
+++ api/src/main/java/com/originit/union/api/config/MvcConfig.java
@@ -1,21 +1,41 @@
 package com.originit.union.api.config;
 
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
-import com.originit.union.api.interceptor.CorsInterceptor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
 
-    @Autowired
-    private CorsInterceptor corsInterceptor;
-
+    /**
+     * 添加跨域
+     */
     @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(corsInterceptor).addPathPatterns("/**");
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowCredentials(true)
+                .allowedHeaders("*")
+                .allowedMethods("*")
+                .allowedOrigins("*");
     }
+
+    /**
+     * 配置fastJson策略
+     */
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        FastJsonHttpMessageConverter converter = new FastJsonHttpMessageConverter();
+        FastJsonConfig config = new FastJsonConfig();
+        config.setSerializerFeatures(SerializerFeature.DisableCircularReferenceDetect,
+                SerializerFeature.WriteMapNullValue);
+        converter.setFastJsonConfig(config);
+        converters.add(converter);
+    }
+
 }
